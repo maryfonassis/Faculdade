bfb55a15d3835c3d3a1d6629fa2c0351188e873f
Mariany Fonseca <marianyf76@gmail.com>
2026-02-17 19:55:51 -0300

    Add files via upload

---
+++ Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double s = 1.0;
+            double x = 3.0;
+            double y = 2.0;
+            while((x <= 99.0) && (y <= 50.0))
+            {
+                s += (x / y);
+                x += 2;
+                y++;
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
