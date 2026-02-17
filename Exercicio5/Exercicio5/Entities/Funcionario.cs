63562e4e4877de6a5d0ed3ae88a6d62759b57eca
Mariany Fonseca <marianyf76@gmail.com>
2026-02-17 20:27:22 -0300

    Add files via upload

---
+++ Exercicio5/Exercicio5/Entities/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercicio5.Entities
+{
+    internal class Funcionario
+    {
+        public double SalarioPorHora { get; set; }
+        public int HorasTrabalhadas { get; set; }
+
+        public Funcionario(double salarioPorHora, int horasTrabalhadas)
+        {
+            SalarioPorHora = salarioPorHora;
+            HorasTrabalhadas = horasTrabalhadas;
+        }
+        public double TotalSalario()
+        {
+            if (HorasTrabalhadas <= (40.0 * 4))
+            {
+                return HorasTrabalhadas * SalarioPorHora;
+            }
+            else
+            {
+                int horasTrabalhadaExtra = HorasTrabalhadas - (40 * 4);
+                double salarioBase = 160 * SalarioPorHora;
+                double salarioExtra = horasTrabalhadaExtra * (SalarioPorHora + (SalarioPorHora * 50 / 100));
+                return salarioExtra + salarioBase;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Horas trabalhadas: {HorasTrabalhadas}, Salario por hora: {SalarioPorHora}, Total a receber {TotalSalario()}";
+        }
+    }
+}
---
+++ Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,19 @@
+﻿using Exercicio5.Entities;
+using System;
+using System.Globalization;
+
+namespace Exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Numero de horas trabalhadas: ");
+            int horas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Salario por hora: ");
+            double salarioHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Funcionario funcionario = new Funcionario(salarioHora, horas);
+            Console.WriteLine(funcionario);
+        }
+    }
+}
