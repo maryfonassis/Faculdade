b8e10ec08861a517b0cd013d7f666670479802a3
Mariany Fonseca <marianyf76@gmail.com>
2026-02-27 16:54:59 -0300

    Add files via upload

---
+++ EstebanEx03/EstebanEx03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstebanEx03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite as 3 notas do aluno:");
+            double nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = ((nota1 * 2) + (nota2 * 3) + (nota3 * 5))/3.0;
+
+            Console.WriteLine($"A média ponderada é: {media}");
+        }
+    }
+}
---
+++ EstebanEx03/EstebanEx03/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("EstebanEx03")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EstebanEx03")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("005bf2aa-1c6e-47e2-8a84-c4997a3f0e4d")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
