c34b06cb4f4509add4e2a73631e72a8da12421ec
Mariany Fonseca <marianyf76@gmail.com>
2026-02-27 17:00:47 -0300

    Add files via upload

---
+++ EstebanEx04/EstebanEx04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EstebanEx04
+{
+    internal class Program
+    
+    {
+        static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número:");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("Par");
+        }
+        else
+        {
+            Console.WriteLine("Impar");
+        }
+    }
+}
+}
