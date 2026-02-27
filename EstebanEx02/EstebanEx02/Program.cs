83fd21a323d16297b767212a802bf69287cda854
Mariany Fonseca <marianyf76@gmail.com>
2026-02-27 16:48:56 -0300

    Add files via upload

---
+++ EstebanEx02/EstebanEx02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static double D(int a, int b, int c)
+        {
+            return (R(a, b) + S(b, c)) / 2;
+        }
+        static double R(int a, int b)
+        {
+            return Math.Pow((a + b), 2);
+        }
+
+        static double S(int b, int c)
+        {
+            return Math.Pow((b + c), 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 3 numeros:");
+            Console.Write("A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("B: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("C: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Resultado: {D(a, b, c).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
