36d7b38446901b900cc31e64c41f083bae2e15a6
Mariany Fonseca <marianyf76@gmail.com>
2026-03-17 19:32:50 -0300

    Add files via upload

---
+++ ExClasseAlunoENota/ExClasseAlunoENota/Entities/Aluno.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExClasseAlunoENota.Entities
+{
+    internal class Aluno
+    {
+        public string RA { get; set; }
+        public string Nome { get; set; }
+        public double NotaProva { get; set; }
+        public double NotaTrabaho { get; set; }
+        public double NotaFinal { private get; set; }
+
+
+        public void CalcularMedia()
+        {
+            NotaFinal = (NotaProva + NotaTrabaho) / 2;
+        }
+        public bool CalcularNotaFinal()
+        {
+            double NotaNecessaria = 14;
+            if ( NotaNecessaria <= (NotaProva + NotaTrabaho))
+            {
+                Console.Write("Passou: ");
+                return true;
+            }
+                Console.WriteLine($"Nota necessaria para passar, {NotaNecessaria - (NotaProva + NotaTrabaho)}");
+                 return false;
+
+        }
+        public double ImprimirNotaFinal()
+        {
+            return NotaFinal;
+        }
+        public void ReceberDados()
+        {
+            Console.Write("RA:");
+            RA = Console.ReadLine();
+            Console.Write("Nome Aluno:");
+            Nome = Console.ReadLine();
+            Console.Write("Nota Prova:");
+            NotaProva = double.Parse(Console.ReadLine());
+            Console.Write("Nota Trabalho:");
+            NotaProva = double.Parse(Console.ReadLine());
+        }
+
+
+
+
+    }
+}
---
+++ ExClasseAlunoENota/ExClasseAlunoENota/Program.cs
@@ -0,0 +1,18 @@
+﻿using ExClasseAlunoENota.Entities;
+using System;
+
+namespace ExClasseAlunoENota
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            aluno.ReceberDados();
+            aluno.CalcularNotaFinal();
+            aluno.CalcularMedia();
+            aluno.ImprimirNotaFinal();
+
+        }
+    }
+}
