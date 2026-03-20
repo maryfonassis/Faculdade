60631ed4cb8d5a05999524850e54b955fea43924
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 15:43:37 -0300

    Add files via upload

---
+++ Becroowd1073/Becroowd1073/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Becroowd1073
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i  % 2 == 0)
+                {
+                    Console.WriteLine($"{i}^2 = {Math.Pow(i, 2)}");
+                }
+            }
+        }
+    }
+}
