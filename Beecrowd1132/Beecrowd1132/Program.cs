5ca1b63a4fdd95ecc990eb553e245811ea079754
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 17:43:45 -0300

    Add files via upload

---
+++ Beecrowd1132/Beecrowd1132/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+
+
+        int soma = 0;
+
+        for (int i = x; i <= y; i++)
+        {
+            if (i % 13 != 0)
+            {
+                soma += i;
+            }
+        }
+
+        Console.WriteLine(soma);
+    }
+}
