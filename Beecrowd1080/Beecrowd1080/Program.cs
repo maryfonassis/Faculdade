60cb6764d3a9fa12aee200106c40d3916fec67ba
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 16:35:03 -0300

    Add files via upload

---
+++ Beecrowd1080/Beecrowd1080/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Beecrowd1080
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maior = int.MinValue;
+            int posicao = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                int valor = int.Parse(Console.ReadLine());
+
+                if (valor > maior)
+                {
+                    maior = valor;
+                    posicao = i;
+                }
+            }
+
+            Console.WriteLine(maior);
+            Console.WriteLine(posicao);
+        }
+    }
+}
