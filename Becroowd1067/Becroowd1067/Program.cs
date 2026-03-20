050b6cbe5dcaff9ec118daf078e4c271077414bc
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 14:31:15 -0300

    Add files via upload

---
+++ Becroowd1067/Becroowd1067/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Becroowd1067
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= x; i++) 
+            { 
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
