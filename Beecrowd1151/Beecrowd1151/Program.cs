942be432b0d2c288cf24d385d0cd5146e05fd1a6
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 19:32:50 -0300

    Add files via upload

---
+++ Beecrowd1151/Beecrowd1151/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == n - 1)
+                Console.Write(a);
+            else
+                Console.Write(a + " ");
+
+            int proximo = a + b;
+            a = b;
+            b = proximo;
+        }
+
+        Console.WriteLine();
+    }
+}
