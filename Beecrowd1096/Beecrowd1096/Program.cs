6391c6ad93a6a3de2e549f7b19972bcbc4f896bd
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 16:55:37 -0300

    Add files via upload

---
+++ Beecrowd1096/Beecrowd1096/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Beecrowd1096
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i= i+2)
+            {
+                for (int j =  7; j >= 5; j--)
+                {
+                    Console.WriteLine($"I={i} J={j}");
+                }
+            }
+        }
+    }
+}
