f8bdb9338cc29b87e965136104ab6e43c3f076ba
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 16:31:25 -0300

    Add files via upload

---
+++ Becroowd1079/Becroowd1079/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Becroowd1079
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+
+                double a = double.Parse(valores[0], CultureInfo.InvariantCulture);
+                double b = double.Parse(valores[1], CultureInfo.InvariantCulture);
+                double c = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+                double media = (a * 2 + b * 3 + c * 5) / 10.0;
+
+                Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+
+            }
+        }
+    }   
+}
