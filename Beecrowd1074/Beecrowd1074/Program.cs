9614801780f87daf2c4da602a7210df5989ce8ae
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 16:16:41 -0300

    Add files via upload

---
+++ Beecrowd1074/Beecrowd1074/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Beecrowd1074
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] vet = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = int.Parse(Console.ReadLine());
+            }
+            string positivoNegativo;
+            string parImpar;
+
+            for (int i = 0; i< n; i++)
+            {
+                if (vet[i] == 0)
+                {
+                    Console.WriteLine("NULL");
+                }
+                else
+                {
+                    parImpar = vet[i] % 2 == 0 ? "EVEN" : "ODD";
+                    positivoNegativo = vet[i] > 0 ? "POSITIVE" : "NEGATIVE";
+                    Console.WriteLine($"{parImpar} {positivoNegativo}");
+                }
+
+            }
+        }
+    }
+}
