6a9fc8a8fec75f003867204b11f42a33ca650494
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 19:27:41 -0300

    Add files via upload

---
+++ Beecrowd1144/Beecrowd1144/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd1144
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int quadrado = i * i;
+                int cubo = i * i * i;
+
+                Console.WriteLine($"{i} {quadrado} {cubo}");
+                Console.WriteLine($"{i} {quadrado + 1} {cubo + 1}");
+            }
+        }
+    }
+}
