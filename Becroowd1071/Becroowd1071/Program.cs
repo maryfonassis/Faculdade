8549eee0c8755450a66678305c06887a4d4f9244
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 15:31:03 -0300

    Add files via upload

---
+++ Becroowd1071/Becroowd1071/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Becroowd1071
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int soma = 0;
+            if (x <= y)
+            {
+                for (int i = x+1; i <= y-1; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        soma += i;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = y+1; i <= x-1; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        soma += i;
+                    }
+
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
