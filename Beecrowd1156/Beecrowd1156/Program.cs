cb8fd172e040073f95b61f02c3df6a5c6d4f6392
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 19:41:26 -0300

    Add files via upload

---
+++ Beecrowd1156/Beecrowd1156/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        double s = 0.0;
+        double denominador = 1.0;
+
+        for (int numerador = 1; numerador <= 39; numerador += 2)
+        {
+            s += numerador / denominador;
+            denominador *= 2.0;
+        }
+
+        Console.WriteLine(s.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
