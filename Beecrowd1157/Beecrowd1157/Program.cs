fea320d14f49870bea72a4869ccc7412133f7561
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 19:43:36 -0300

    Add files via upload

---
+++ Beecrowd1157/Beecrowd1157/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
