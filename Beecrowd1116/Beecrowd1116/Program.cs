26e10fa248a421a02f5cfe2f6cfe483e8bc2c34b
Mariany Fonseca <marianyf76@gmail.com>
2026-03-20 17:38:04 -0300

    Add files via upload

---
+++ Beecrowd1116/Beecrowd1116/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd1116
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                int x = int.Parse(vet[0]);
+                int y = int.Parse(vet[1]);
+
+                if (y == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    double resultado = (x / (y * 1.0));
+                    Console.WriteLine(resultado.ToString("F1", CultureInfo.InvariantCulture));
+                }
+
+
+            }
+            
+        }
+    }
+}
