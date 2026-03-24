2955cb6839f91cb5b615b246cbf4146c779f0358
Mariany Fonseca <marianyf76@gmail.com>
2026-03-24 19:44:04 -0300

    Add files via upload

---
+++ ExClasseContaBancaria/ExClasseContaBancaria/Program.cs
@@ -0,0 +1,22 @@
+﻿using ExClasseContaBancaria.Entities;
+using System;
+using System.Globalization;
+
+namespace ExClasseContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria();
+            contaBancaria.ReceberDados();
+            Console.Write("Quanto deseja depositar: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Deposita(deposito);
+            Console.Write("Quanto deseja sacar: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Sacar(saque);
+            contaBancaria.MostrarSaldo();
+        }
+    }
+}
