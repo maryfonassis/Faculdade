402b328b52b478f5e8f7df1dbc9f2c3959ab5626
Mariany Fonseca <marianyf76@gmail.com>
2026-03-27 15:58:16 -0300

    Add files via upload

---
+++ VetoresEx06/VetoresEx06/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace VetoresEx06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] salarios = new double[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Salário do funcionário {i + 1}: ");
+                salarios[i] = double.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                salarios[i] *= 1.05;
+            }
+
+            Console.WriteLine("Salários reajustados:");
+
+            foreach (double s in salarios)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
