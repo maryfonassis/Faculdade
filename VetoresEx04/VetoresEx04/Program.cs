7f4703817e37ec1553daa6378fb3ec9eab6ace42
Mariany Fonseca <marianyf76@gmail.com>
2026-03-27 15:55:19 -0300

    Add files via upload

---
+++ VetoresEx04/VetoresEx04/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace VetoresEx04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v1 = new int[10];
+            int[] v2 = new int[10];
+            int[] resultado = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"V1 - Digite o número {i + 1}: ");
+                v1[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"V2 - Digite o número {i + 1}: ");
+                v2[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                resultado[i] = v1[i] + v2[i];
+            }
+
+            Console.WriteLine("Vetor resultante:");
+            foreach (int num in resultado)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
