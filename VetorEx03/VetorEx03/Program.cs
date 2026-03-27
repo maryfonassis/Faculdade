bb7e7cc48655920456748ca9e5c42a1583b6d7bf
Mariany Fonseca <marianyf76@gmail.com>
2026-03-27 15:53:29 -0300

    Add files via upload

---
+++ VetorEx03/VetorEx03/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace VetorEx03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[8];
+            bool encontrou = false;
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write($"Digite o número {i + 1}: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Digite um número para buscar: ");
+            int busca = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (vetor[i] == busca)
+                {
+                    Console.WriteLine($"Encontrado na posição {i}");
+                    encontrou = true;
+                    break;
+                }
+            }
+
+            if (!encontrou)
+            {
+                Console.WriteLine("O número não se encontra no vetor");
+            }
+        }
+    }
+}
