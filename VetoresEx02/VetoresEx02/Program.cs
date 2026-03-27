aa1030699e7866ca27c8a1f0b0328284edbaf93f
Mariany Fonseca <marianyf76@gmail.com>
2026-03-27 15:50:52 -0300

    Add files via upload

---
+++ VetoresEx02/VetoresEx02/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace VetoresEx02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[15];
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write($"Digite o número {i + 1}: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Números positivos:");
+
+            for (int i = 0; i < 15; i++)
+            {
+                if (vetor[i] > 0)
+                {
+                    Console.WriteLine(vetor[i]);
+                }
+            }
+        }
+    }
+}
