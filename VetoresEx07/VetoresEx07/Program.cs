fb182e39b4c4bac5bdd214bee7f988cbcc933c2e
Mariany Fonseca <marianyf76@gmail.com>
2026-03-27 16:01:59 -0300

    Add files via upload

---
+++ VetoresEx07/VetoresEx07/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace VetoresEx07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor1 = new int[10];
+            int[] vetor2 = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Digite o número {i + 1}: ");
+                vetor1[i] = int.Parse(Console.ReadLine());
+            }
+
+            int j = 0;
+
+            for (int i = vetor1.Length - 1; i >= 0; i--)
+            {
+                vetor2[j] = vetor1[i];
+                j++;
+            }
+
+            Console.WriteLine("Vetor invertido:");
+
+            foreach (int num in vetor2)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
